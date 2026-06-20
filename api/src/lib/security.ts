7057ff7e78ebaafd105d48cb109fbd9ad3faf7dc
omclabs <omclabs@gmail.com>
2026-06-20 08:54:56 +0700

    chore: ignore local env and wrangler config

--- api/src/lib/security.ts
+++ api/src/lib/security.ts
@@ -3,6 +3,8 @@ const DEV_ALLOWED_ORIGINS = [
   'http://127.0.0.1:5173',
   'http://localhost:4173',
   'http://127.0.0.1:4173',
+  'http://localhost:5787',
+  'http://127.0.0.1:5787',
 ];
 
 const DEFAULT_ADMIN_PASSWORD_HASH =
